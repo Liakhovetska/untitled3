3295a7b12470eab9ff2a97475685c430bb627ca3
yanaliakhovetska <Lyahovetskayay@gmail.com>
2024-05-12 20:53:59 +0300

    Lesson 9

---
+++ src/lesson9.2.1.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/lesson9.2.2.php';
+
+$taskList = new TaskGenerator();
+$taskList->addTask('Complete lawyer agreement', 3);
+$taskList->addTask('Learning php', 1);
+$taskList->addTask('Complete design project', 2);
+
+$tasks = $taskList->getTasks();
+echo "Current Tasks:" . PHP_EOL;
+$taskIdToComplete = key($tasks);
+$taskList->completeTask($taskIdToComplete);
+echo "Task '{$tasks[$taskIdToComplete]['name']}' marked as completed." . PHP_EOL;
+
+$tasks = $taskList->getTasks();
+echo "Updated Tasks:" . PHP_EOL;
+foreach ($tasks as $taskId => $task) {
+    echo "- {$task['name']} (Priority: {$task['priority']}, State: {$task['state']})" . PHP_EOL;;
+}
---
+++ src/lesson9.2.2.php
@@ -0,0 +1,91 @@
+<?php
+
+class TaskGenerator
+{
+    const COMPLETED = 'виконано';
+    const NOT_COMPLETED = 'не виконано';
+    private array $tasks = [];
+    private string $fileName = 'tasks.txt';
+
+    public function __construct()
+    {
+        $this->loadTasksFromFile();
+    }
+
+    /**
+     * @param string $taskName
+     * @param int $priority
+     * @return void
+     */
+    public function addTask(string $taskName, int $priority)
+    {
+        $taskId = uniqid();
+        $this->tasks[$taskId] = ['name' => $taskName, 'priority' => $priority, 'state' => self::NOT_COMPLETED];
+        $this->saveTasksToFile();
+    }
+
+    /**
+     * @param int $taskId
+     * @return bool
+     */
+    public function deleteTask(int $taskId)
+    {
+        if (isset($this->tasks[$taskId])) {
+            unset($this->tasks[$taskId]);
+            $this->saveTasksToFile();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTasks()
+    {
+        $sortedTasks = $this->tasks;
+        usort($sortedTasks, function ($a, $b) {
+            return $b['priority'] - $a['priority'];
+        });
+
+        foreach ($sortedTasks as $taskId => $task) {
+            echo "- {$task['name']} (Priority: {$task['priority']}, State: {$task['state']})" . PHP_EOL;;
+        }
+
+        return $sortedTasks;
+    }
+
+    /**
+     * @param int $taskId
+     * @return bool
+     */
+    public function completeTask(int $taskId)
+    {
+        if (isset($this->tasks[$taskId])) {
+            $this->tasks[$taskId]['state'] = self::COMPLETED;
+            var_dump( $this->tasks).die();
+            $this->saveTasksToFile();
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return void
+     */
+    private function loadTasksFromFile()
+    {
+        if (file_exists($this->fileName)) {
+            $this->tasks = unserialize(file_get_contents($this->fileName));
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function saveTasksToFile()
+    {
+        file_put_contents($this->fileName, serialize($this->tasks));
+    }
+}
